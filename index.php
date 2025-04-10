9d19fca6b44047795907aaa7b3caf26624ec4f1a
yoonbe <146192523+yoonbe@users.noreply.github.com>
2025-04-10 08:22:11 -0300

    VAIIIII!!!!!!!!!!!!

--- index.php
+++ index.php
@@ -4,9 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="script.js"></script>
     <title>Schelp</title>
 </head>
 <body>
-    
+    <!-- Overlay -->
+    <div class="overlay" id="overlay" onclick="closeSidebar()"></div>
+
+    <!-- Navbar -->
+    <div class="navbar">
+        <span class="menu-icon" onclick="openSidebar()">☰</span>
+    </div>
+
+    <!-- Sidebar -->
+    <div id="sidebar" class="sidebar">
+        <a href="#">Início</a>
+        <a href="#">Tarefas</a>
+        <a href="#">Conteúdos</a>
+        <a href="#">Configurações</a>
+        <a href="#">Feedback</a>
+    </div>
 </body>
 </html>
