50dabc27f3dc53688c52021cd2ebc48346582b92
yoonbe <146192523+yoonbe@users.noreply.github.com>
2025-04-10 08:35:24 -0300

    sad

---
+++ configuracoes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="JS/script.js"></script>
+    <title>Schelp</title>
+</head>
+<body>
+    <!-- Overlay -->
+    <div class="overlay" id="overlay" onclick="closeSidebar()"></div>
+
+    <!-- Navbar -->
+    <div class="navbar">
+        <span class="menu-icon" onclick="openSidebar()">☰</span>
+    </div>
+
+    <!-- Sidebar -->
+    <div id="sidebar" class="sidebar">
+        <a href="index.php">Início</a>
+        <a href="tarefas.php">Tarefas</a>
+        <a href="#">Conteúdos</a>
+        <a href="#">Configurações</a>
+        <a href="#">Feedback</a>
+    </div>
+</body>
+</html>
