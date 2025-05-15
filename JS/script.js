ac20b8b393379b51c62d922c765626f03c1b87ab
Ludovico-Pitoco <ludovicopitoco@hotmail.com>
2025-05-15 08:34:04 -0300

    v2.5

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,21 @@ function openSidebar() {
     document.getElementById("sidebar").style.left = "-250px";
     document.getElementById("overlay").style.display = "none";
   }
+
+  var tema = "Claro";
+  var cor = "#FFFFFF"
+
+  function mudartema(body) {
+    if (tema == "Claro")
+    {
+      cor = "#000000";
+      tema = "Escuro";
+    }
+    else{
+      cor = "#FFFFFF";
+      tema = "Claro";
+    }
+    body.style.backgroundColor = cor;
+}
+
+    
