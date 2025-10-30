cc38e4155cc3949c351cbdcddf88a85746d6bf71
Naoki0g <jv2019.6012@gmail.com>
2025-10-29 23:29:44 -0300

    Add files via upload

--- rodape.php
+++ rodape.php
@@ -1,5 +1,6 @@
 <script src="Script/script.js"></script>
 <script src="Script/solucaologout.js"></script>
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 </body>
 </html>
